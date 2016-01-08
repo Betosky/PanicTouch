2ee9a2f5268f5705211a106ad4c87deac5a08503
Alberto Paillao <albertopaillao@gmail.com>
2016-01-08 03:32:01 -0300

    asdasd

--- app/src/main/java/redes/opencode/com/panictouch/Mensaje.java
+++ app/src/main/java/redes/opencode/com/panictouch/Mensaje.java
@@ -5,18 +5,37 @@ import android.os.Bundle;
 import android.telephony.SmsManager;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Mensaje extends AppCompatActivity {
-Button btnMsje;
+    private EditText nFono;
+    private EditText msje;
+    private Button btnMsje;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mensaje);
 
+        nFono = (EditText)findViewById(R.id.editText);
+        msje = (EditText)findViewById(R.id.editText2);
+
         btnMsje = (Button)findViewById(R.id.btnMsje);
         btnMsje.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                sendSMS("+56988678358", "Ayuda, estoy en peligro.");
+                String numero = nFono.getText().toString();
+                String sms = msje.getText().toString();
+
+                try{
+                    sendSMS(numero,sms);
+                    Toast.makeText(getApplicationContext(), "SMS enviado",
+                            Toast.LENGTH_LONG).show();
+                } catch (Exception e){
+                    Toast.makeText(getApplicationContext(),"SMS fallido. El mensaje no ha sido enviado",
+                            Toast.LENGTH_LONG).show();
+                    e.printStackTrace();
+                }
             }
         });
     }
